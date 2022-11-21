c4aeb2abceb5ba4d8c5a823c02f112f10ab65fbe
Alastair <alastair.chau@gmail.com>
2022-11-21 13:02:27 +0000

    linked model class to index file for webpage

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const NotesModel = require('./notesModel')
+
+
+
 console.log('The notes app is running')
+
+
+
+const model = new NotesModel();
+console.log(model.getNotes());
+model.addNote('hello console log!')
+model.addNote('bye console log!')
+
+console.log(model.getNotes());
+
